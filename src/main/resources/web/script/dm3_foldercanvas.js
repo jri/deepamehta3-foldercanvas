84ef9447c331e7725b17a3c641d9eab910669b34
Jörg Richter <jri@deepamehta.de>
2010-08-19 00:36:14 +0200

    Initial commit. Functional. Still missing: 1) Synchronization with original folder, 2) Grid-aligned positioning of created file and folder topics.

---
+++ src/main/resources/web/script/dm3_foldercanvas.js
@@ -0,0 +1,31 @@
+function dm3_foldercanvas() {
+
+
+
+    /**************************************************************************************************/
+    /**************************************** Overriding Hooks ****************************************/
+    /**************************************************************************************************/
+
+
+
+    this.process_files_drop = function(files) {
+        if (files.get_directory_count() != 1 || files.get_file_count() != 0) {
+            alert("WARNING: dropped items ignored.\n\nTo create a Folder Canvas drop a single folder here.")
+            return
+        }
+        //
+        var dir = files.get_directory(0)
+        get_plugin("dm3_topicmaps").create_topicmap(dir.name)
+        //
+        get_plugin("dm3_files").create_file_topics(dir)
+    }
+
+
+
+    /************************************************************************************************/
+    /**************************************** Custom Methods ****************************************/
+    /************************************************************************************************/
+
+
+
+}
