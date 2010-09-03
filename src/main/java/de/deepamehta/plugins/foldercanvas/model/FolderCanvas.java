d51325b72d6174db2d5df7586b3d4f594053093e
Jörg Richter <jri@deepamehta.de>
2010-09-03 18:24:33 +0200

    First synchronization functionality (not yet complete). Relies on the services of the topicmaps and files plugins.

--- src/main/java/de/deepamehta/plugins/foldercanvas/model/FolderCanvas.java
+++ src/main/java/de/deepamehta/plugins/foldercanvas/model/FolderCanvas.java
@@ -1,5 +1,7 @@
 package de.deepamehta.plugins.foldercanvas.model;
 
+import de.deepamehta.plugins.files.FilesPlugin;
+import de.deepamehta.plugins.topicmaps.TopicmapsPlugin;
 import de.deepamehta.plugins.topicmaps.model.Topicmap;
 
 import de.deepamehta.core.model.RelatedTopic;
@@ -23,6 +25,13 @@ import java.util.logging.Logger;
 
 public class FolderCanvas extends Topicmap {
 
+    // ---------------------------------------------------------------------------------------------- Instance Variables
+
+    private TopicmapsPlugin topicmapsPlugin = (TopicmapsPlugin) dms.getPlugin("de.deepamehta.3-topicmaps");
+    private FilesPlugin filesPlugin         = (FilesPlugin)     dms.getPlugin("de.deepamehta.3-files");
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
     // ---------------------------------------------------------------------------------------------------- Constructors
 
     public FolderCanvas(long topicmapId, CoreService dms) {
@@ -31,12 +40,32 @@ public class FolderCanvas extends Topicmap {
 
     // -------------------------------------------------------------------------------------------------- Public Methods
 
-    public void synchronize() {
+    public void synchronize() throws Exception {
         Topic folderTopic = getFolderTopic();
-        String path = (String) dms.getTopicProperty(folderTopic.id, "de/deepamehta/core/property/Path");
-        for (File file : new File(path).listFiles()) {
-            file.getPath();
+        String syncPath = (String) dms.getTopicProperty(folderTopic.id, "de/deepamehta/core/property/Path");
+        SyncStats syncStats = new SyncStats();
+        for (File file : new File(syncPath).listFiles()) {
+            String path = file.getPath();
+            Topic topic = dms.getTopic("de/deepamehta/core/property/Path", path);
+            if (topic != null) {
+                if (!containsTopic(topic.id)) {
+                    topicmapsPlugin.addTopicToTopicmap(topic.id, 0, 0, topicmapId); // FIXME: positioning
+                    syncStats.countAsAdded(file);
+                }
+            } else {
+                if (file.isDirectory()) {
+                    topic = filesPlugin.createFolderTopic(path);
+                } else {
+                    topic = filesPlugin.createFileTopic(path);
+                }
+                topicmapsPlugin.addTopicToTopicmap(topic.id, 0, 0, topicmapId);     // FIXME: positioning
+                syncStats.countAsAdded(file);
+            }
         }
+        // TODO: remove topics from map
+        logger.info("### Synchronization of " + syncPath + " with topicmap " + topicmapId + " complete =>\n" +
+            syncStats.filesAddedCount + " files added\n" +
+            syncStats.foldersAddedCount + " folders added");
     }
 
     // ------------------------------------------------------------------------------------------------- Private Methods
@@ -46,17 +75,28 @@ public class FolderCanvas extends Topicmap {
             asList("de/deepamehta/core/topictype/Folder"), asList("FOLDER_CANVAS;OUTGOING"), null);
         // error check
         if (relTopics.size() == 0) {
-            throw new RuntimeException("Topic Map " + topicmapId +
-                " is not related to any Folder topic");
+            throw new RuntimeException("Topicmap " + topicmapId + " is not related to any Folder topic");
         } else if (relTopics.size() > 1) {
-            throw new RuntimeException("Ambiguity: Topic Map " + topicmapId +
-                " is related to more than one Folder topic");
+            throw new RuntimeException("Ambiguity: topicmap " + topicmapId + " is related to more than one " +
+                "Folder topic");
         }
         //
         return relTopics.get(0).getTopic();
     }
 
-    private boolean isContainedInTopicmap(File file) {
-        return false;
+    // --------------------------------------------------------------------------------------------- Private Inner Class
+
+    private class SyncStats {
+
+        int filesAddedCount = 0;
+        int foldersAddedCount = 0;
+
+        void countAsAdded(File file) {
+            if (file.isDirectory()) {
+                foldersAddedCount++;
+            } else {
+                filesAddedCount++;
+            }
+        }
     }
 }
